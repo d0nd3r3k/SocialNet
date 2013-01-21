31a1577971eb0b5b62d999dc878df8ded47af13c
Apple <Apple@donald.lan>
2013-01-21 15:17:05 +0100

    Authentication

---
+++ models/Account.js
@@ -0,0 +1,146 @@
+module.exports = function(config, mongoose, nodemailer){
+    
+    var crypto = require('crypto');
+    
+    var AccountSchema = new mongoose.Schema({
+        email: {
+            type: String, 
+            unique:true
+        },
+        password: {
+            type: String
+        },
+        name: {
+            first: {
+                type: String
+            },
+            last: {
+                type: String
+            }
+        },
+        birthday:{
+            day: {
+                type: Number, 
+                min: 1, 
+                max: 31, 
+                require: false
+            },
+            month: {
+                type: Number, 
+                min: 1, 
+                max: 12, 
+                require: false
+            },
+            year: {
+                type: Number
+            }
+        },
+        photoUrl: {
+            type: String
+        },
+        biography: {
+            type: String
+        }
+    });
+    
+    var Account = mongoose.model('Account', AccountSchema);
+    
+    var registerCallback = function(err){
+        if(err){
+            return console.log(err);
+        };
+        return console.log('Account was created');
+    };
+    
+    var changePassword = function(accountId, newpassword){
+        var shaSum = crypto.createHash('sha256');
+        shaSum.update(newpassword);
+        
+        var hashedPassword = shaSum.digest('hex');
+        Account.update(
+        {
+            _id:accountId
+        }, 
+
+        {
+            $set: {
+                password:hashedPassword
+            }
+        },
+
+        {
+            upsert:false
+        }, 
+        function changePasswordCallback(err){
+            console.log('Change Password done for account ' + accountId); 
+        });
+    };
+
+    var forgotPassword = function(email, resetPasswordUrl, callback){
+        var user = Account.findOne({
+            email:email
+        }, function findAccount(err, doc){
+            if(err){
+                // Email address is not a valid user
+                callback(false);
+            }
+            else{
+                var smtpTransport = nodemailer.createTransport('SMTP', config.mail);
+                resetPasswordUrl += '?account=' + doc._id;
+                smtpTransport.sendMail({
+                    from:'SocialNet@donaldderek.com',
+                    to: doc.email,
+                    subject: 'SocialNet Password Request',
+                    text: 'Click here to reset your password: ' + resetPasswordUrl
+                },
+                function forgotPasswordResult(err){
+                    if(err){
+                        callback(false);
+                    }
+                    else{
+                        callback(true);
+                    }
+                });
+            }
+        });
+    };
+    
+    var login = function(email, password, callback){
+      
+      var shaSum = crypto.createHash('sha256');
+      shaSum.update(password);
+      
+      Account.findOne({
+         email:email,
+         password:shaSum.digest('hex')
+      },
+      function(err){
+          callback(null != doc);
+      }); 
+    };
+    
+    var register = function (email, password, firstName, lastName){
+        var shaSum = crypto.createHash('sha256');
+        shaSum.update(password);
+        
+        console.log('Registering ' + email);
+        var user = new Account({
+           email: email,
+           name:{
+               first: firstName,
+               last: lastName
+           },
+           password: shaSum.digest('hex')
+        });
+        user.save(registerCallback);
+        console.log('Save command was Sent');
+    }
+    
+    return {
+        register: register,
+        forgotPassword: forgotPassword,
+        changePassword: changePassword,
+        login: login,
+        Account: Account
+    }
+}
---
+++ public/js/router.js
@@ -0,0 +1,37 @@
+define(['views/index', 'views/register', 'views/login', 'views/forgotpassword'],
+    function(IndexView, RegisterView, LoginView, ForgotPasswordView){
+    
+        var SocialRouter = Backbone.Router.extend({
+            
+            currentView: null,
+            routes:{
+                "index": "index",
+                "login": "login",
+                "register": "register",
+                "forgotpassword": "forgotpassword"
+            },
+            
+            changeView: function(view){
+                if( null != this.currentView ){
+                    this.currentView.undelegateEvents();
+                }
+                this.currentView = view;
+                this.currentView.render();
+            },
+            
+            index: function(){
+                this.changeView( new IndexView());
+            },
+            login: function(){
+                this.changeView( new LoginView());
+            },
+            forgotpassword: function(){
+                this.changeview( new ForgotPasswordView());
+            },
+            register: function(){
+                this.changeview ( new RegisterView());
+            }
+        });
+        
+        return new SocialRouter();
+    });
