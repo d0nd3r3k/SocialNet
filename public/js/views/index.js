8d5f5220f90484af8207ac674324235d93a3ceff
Apple <Apple@donald.lan>
2013-01-21 14:30:28 +0100

    Configuration

---
+++ public/js/views/index.js
@@ -0,0 +1,13 @@
+define(['text!templates/index.html'], function(indexTemplate){
+   
+   var indexView = Backbone.View.extend({
+       el: $('#content'),
+       
+       render: function(){
+           this.$el.html(indexTemplate);
+       }
+       
+   });
+   
+   return new indexView;
+});
